5440a0d0000db3ab9d4bc5fcf0aece119870f6f0
dre <alipson@pm.me>
2024-07-27 21:31:36 -0700

    basic helper with region pair and quad types

--- creatives/creative_3_mondrian-art/src/main/java/me/alipson/mondrian/Mondrian.java
+++ creatives/creative_3_mondrian-art/src/main/java/me/alipson/mondrian/Mondrian.java
@@ -13,23 +13,42 @@ public class Mondrian {
     private int height;
     private int width;
 
+    private Color[][] pixels;
+
+    private static final Color[] COLORS = { Color.RED, Color.YELLOW, Color.CYAN, Color.WHITE };
+    private static final int SPLIT_BUFFER = 10;
+
     /**
      */
     public void paintBasicMondrian(Color[][] pixels) {
         this.height = pixels.length;
         this.width = pixels[0].length;
+        this.pixels = pixels;
 
-        basicHelper(new ColorMatrix(pixels));
+        basicHelper(
+                new Region(new Point(0, width), new Point(0, height)),
+                new Random());
     }
 
     /**
      */
-    private void basicHelper(ColorMatrix pixels) {
-        if (pixels.isMinimalCell()) { // fill cell
-            // fill();
-        } else if (pixels.isQuarterWidth()) {
-
-        } else if (pixels.isQuarterHeight()) {
+    private void basicHelper(Region region, Random rand) {
+        if (region.isQuarterWidth() && region.isQuarterHeight()) { // fill cell
+            region.fill(randomBasicColor(rand));
+        } else if (region.isQuarterWidth()) { // divide horizontally
+            Region.RegionPair newRegions = region.splitHorizontal(rand);
+            basicHelper(newRegions.first, rand);
+            basicHelper(newRegions.second, rand);
+        } else if (region.isQuarterHeight()) { // divide vertically
+            Region.RegionPair newRegions = region.splitVertical(rand);
+            basicHelper(newRegions.first, rand);
+            basicHelper(newRegions.second, rand);
+        } else { // divide horizontally and vertically
+            Region.RegionQuad newRegions = region.splitQuadrants(rand);
+            basicHelper(newRegions.topLeft, rand);
+            basicHelper(newRegions.topRight, rand);
+            basicHelper(newRegions.botLeft, rand);
+            basicHelper(newRegions.botRight, rand);
         }
     }
 
@@ -39,69 +58,122 @@ public class Mondrian {
     }
 
     /**
-     * Fills a image matrix of Color pixels with a white rectangular region starting
-     * from
-     * the position (x1,y1) inclusive to (x2,y2) exclusive.
-     *
-     * @param pixels the starting Color[][] pixel image matrix.
-     * @param x1     the left-most x coordinate int
-     * @param x2     the right-most x coordinate int
-     * @param y1     the top-most y coordinate int
-     * @param y2     the bottom-most y coordinate int
      */
-    public static void fill(Color[][] pixels, int x1, int x2, int y1, int y2) {
-        for (int y = y1 + 1; y < y2 - 1; y++) {
-            for (int x = x1 + 1; x < x2 - 1; x++) {
-                pixels[y][x] = Color.WHITE;
-            }
-        }
+    private Color randomBasicColor(Random rand) {
+        return COLORS[rand.nextInt(COLORS.length)];
     }
 
     /**
+     * Region wraps a 2D Color matrix array of Color[][] in addition to
+     * two Point points which form a region of operation for a Picture object's
+     * Color[][] martix.
+     * The class provides methods for accessing the height and width of the pixel
+     * matrix image,
      */
-    private class Vec2 {
-        int x;
-        int y;
+    private class Region {
+        Point topLeft;
+        Point botRight;
+
+        public Region(Point topLeft, Point botRight) {
+            this.topLeft = topLeft;
+            this.botRight = botRight;
+        }
+
+        // FIX: way to not create so many new objects?
+        public RegionPair splitHorizontal(Random rand) {
+            int midY = chooseRandomHorizontalDivider(rand);
+            return new RegionPair(
+                    new Region(topLeft, new Point(botRight.x, midY)),
+                    new Region(new Point(topLeft.x, midY), botRight));
+        }
+
+        public RegionPair splitVertical(Random rand) {
+            int midX = chooseRandomVerticalDivider(rand);
+            return new RegionPair(
+                    new Region(topLeft, new Point(midX, botRight.y)),
+                    new Region(new Point(midX, topLeft.y), botRight));
+        }
+
+        public RegionQuad splitQuadrants(Random rand) {
+            int midX = chooseRandomVerticalDivider(rand);
+            int midY = chooseRandomHorizontalDivider(rand);
+            return new RegionQuad(
+                    new Region(topLeft, new Point(midX, midY)),
+                    new Region(new Point(midX, topLeft.y), new Point(botRight.x, midY)),
+                    new Region(new Point(topLeft.x, midY), new Point(midX, botRight.y)),
+                    new Region(new Point(midX, midY), botRight));
+        }
+
+        private int chooseRandomHorizontalDivider(Random rand) {
+            return chooseRandomSubregion(topLeft.y, botRight.y, rand);
+        }
+
+        private int chooseRandomVerticalDivider(Random rand) {
+            return chooseRandomSubregion(topLeft.x, botRight.x, rand);
+        }
 
         /**
          */
-        public Vec2(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-    /**
-     * ColorMatrix is a wrapper class for a 2D Color matrix array, Color[][],
-     * and provides methods for accessing the height and width of the pixel matrix
-     * image.
-     */
-    private class ColorMatrix {
-        Color[][] pixels;
-
-        /**
-         */
-        public ColorMatrix(Color[][] pixels) {
-            this.pixels = pixels;
+        public int chooseRandomSubregion(int min, int max, Random rand) {
+            // + 1 to consider inclusive max
+            return rand.nextInt(min + SPLIT_BUFFER, max - SPLIT_BUFFER + 1);
         }
 
         /**
          */
-        public boolean isMinimalCell() {
-            return isQuarterWidth() && isQuarterHeight();
-        }
+        // public boolean isMinimalCell() {
+        // return isQuarterWidth() && isQuarterHeight();
+        // }
 
         /**
          */
         public boolean isQuarterHeight() {
-            return pixels[0].length < height;
-
+            return botRight.y - topLeft.y < height;
         }
 
         /**
          */
         public boolean isQuarterWidth() {
-            return pixels.length < width;
+            return botRight.x - topLeft.x < width;
+        }
+
+        /**
+         * Fills the current region of the Mondrian pixels with a given Color color,
+         * starting from topLeft inclusive to botRight exclusive,
+         * leaving a one pixel border around the region.
+         *
+         * @param color the Color color to fill
+         */
+        public void fill(Color color) {
+            for (int y = topLeft.y + 1; y < botRight.y - 1; y++) {
+                for (int x = topLeft.x + 1; x < botRight.x - 1; x++) {
+                    pixels[y][x] = color;
+                }
+            }
+        }
+
+        class RegionPair {
+            public Region first;
+            public Region second;
+
+            public RegionPair(Region first, Region second) {
+                this.first = first;
+                this.second = second;
+            }
+        }
+
+        class RegionQuad {
+            public Region topLeft;
+            public Region topRight;
+            public Region botLeft;
+            public Region botRight;
+
+            public RegionQuad(Region topLeft, Region topRight, Region botLeft, Region botRight) {
+                this.topLeft = topLeft;
+                this.topRight = topRight;
+                this.botLeft = botLeft;
+                this.botRight = botRight;
+            }
         }
     }
 }
---
+++ creatives/creative_3_mondrian-art/src/main/java/me/alipson/mondrian/Vec2.java
@@ -0,0 +1,21 @@
+package me.alipson.mondrian;
+
+// Alexandre Lipson
+// CSE 123
+// 07/26/24
+// C3: Mondrian Art
+// TA: Daniel
+
+/**
+ */
+public class Vec2 {
+    int x;
+    int y;
+
+    /**
+     */
+    public Vec2(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
